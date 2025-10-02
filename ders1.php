577c7a7c33253b6f0ed63a02937a64211d20b09c
Furkan <furkanzorlu343734@gmail.com>
2025-10-02 11:11:02 +0300

    php
    php

---
+++ ders1.php
@@ -0,0 +1,50 @@
+<html>
+<body>
+
+
+<?php
+#define("kulanıcı", "Furkan");
+#define("admin","Serbun");
+#echo "Bu sitenin kulanıcısı:";
+#echo kulanıcı;
+#echo "<br> Bu sitenin admini:";
+#echo admin
+
+/*
+$a="Kastamonu";
+$b="Meslek";
+$c="Yüksekokulu";
+$Birlesim=$a." ".$b." ".$c;
+echo $Birlesim;
+*/
+/*
+$Ad="Furkan";
+$Soyad="Zorlu";
+$adsoyad=$Ad." ".$Soyad;
+echo adsoyad;*/
+/*
+$firstone=10;
+$secondone=2;
+$toplam= $firstone+$secondone;
+echo $toplam. "<br>";
+$cıkarım= $firstone-$secondone;
+echo $cıkarım. "<br>";
+$carp= $firstone*$secondone;
+echo $carp. "<br>";
+$bolm= $firstone/$secondone;
+echo $bolm. "<br>";
+$kalan= $firstone%$secondone;
+echo $kalan. "<br>";
+*/
+/*
+$x=10;
+$y=5;
+$y+=$x;
+echo $y;
+*/
+
+
+
+
+?>
+</body> </html>
